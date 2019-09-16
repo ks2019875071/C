9ccf04a8ac612ead39fe468e6e55daa1499c2ef8
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2019-09-16 16:19:59 +0900

    Add files via upload

--- test16.c
+++ test16.c
@@ -3,12 +3,16 @@
 int main()
 {
 	char name1[31], name2[31];
-	scanf("%s %s", name1, name2);
+	scanf("%s", name1);
+	scanf("%s", name2);
 	
 	int count1 = strlen(name1), count2 = strlen(name2);
 	
-	printf("%s %s/n", name1, name2);
-	printf("%d%-d", count1, count2);
+	printf("%*s %*s\n", count1, name1, count2, name2);
+	printf("%*d %*d\n", count1, count1, count2, count2);
+	
+	printf("%*s %*s\n", count1, name1, count2, name2);
+	printf("%-*d %-*d", count1, count1, count2, count2);
 	
 	return 0;
 }
