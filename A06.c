a769de8a4a23729a7bee1ae248df4a50ffe06ec4
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2019-11-25 15:06:09 +0900

    Add files via upload

--- A06.c
+++ A06.c
@@ -4,26 +4,21 @@
 int main()
 {
 	char ch[31];
-	char c;
-	int i;
 	
 	scanf("%s", ch);
 	
-	for (i = 0 ; i< strlen(ch); i++)
-	{
-		
-	}
+	int i = strlen(ch);
 	
 	int l, j, k, m;
 	
-	for (j=0; j<i-1; j++)
+	for (j=0; j<i; j++)
 	{
 		for (l=0; l<j; l++)
 		{
 			printf("-");
 		}
 		
-		for (k=j; k<i-2; k++)
+		for (k=j; k<i; k++)
 		{
 			printf("%c", ch[k]);
 		}
